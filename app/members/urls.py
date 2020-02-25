346f8cdd01b8d29213b6a6cb97927ba70a8a3321
JEONGSUJI <wtw9611@gmail.com>
2020-02-25 22:05:31 +0900

    url 연결 및 html 약간 수정

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from members.views import login_view, user_setting_view
+from members.views import login_view, user_setting_view, logout_view
 
 app_name = 'members'
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('user-setting/', user_setting_view, name='user_setting'),
+    path('login-out/', logout_view, name='login-out'),
+    path('user-setting/', user_setting_view, name='user-setting'),
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 
 def login_view(request):
-    return render(request, 'members/login.html')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('members:user-setting')
+    else:
+        return redirect('members:login')
 
 
 def user_setting_view(request):
     return render(request, 'members/user_setting.html')
+
+
+def logout_view(request):
+    logout(request)
