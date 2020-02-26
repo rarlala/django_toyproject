3d65007de94a46c40cecd394eac39c1fb949fabe
JEONGSUJI <wtw9611@gmail.com>
2020-02-26 15:51:23 +0900

    html 약간 수정 및 login 기능 구현, user_setting 화면 초안 완성

--- app/members/views.py
+++ app/members/views.py
@@ -3,14 +3,17 @@ from django.shortcuts import render, redirect
 
 
 def login_view(request):
-    username = request.POST['username']
-    password = request.POST['password']
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect('members:user-setting')
+    if request.method == 'POST':
+        # email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('members:user-setting')
     else:
-        return redirect('members:login')
+        return render(request, 'members/login.html')
 
 
 def user_setting_view(request):
