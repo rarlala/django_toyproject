e1781644b89fa40992e81ba2e14fdb4996c616a6
JEONGSUJI <wtw9611@gmail.com>
2020-02-26 17:31:54 +0900

    mask 정보 저장을 위한 model 생성

--- app/companys/models.py
+++ app/companys/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=20)
     address = models.TextField()
+
+class Info(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    desc = models.TextField()
+    price = models.CharField(max_length=20)
+    soldout = models.BooleanField(default=True)
