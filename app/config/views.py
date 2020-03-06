8ee90de5c49e2d7ddab74d427519c35ce7ddf7dc
JEONGSUJI <wtw9611@gmail.com>
2020-03-06 22:32:15 +0900

    유저 서비스 신청 화면 초안

--- app/config/views.py
+++ app/config/views.py
@@ -1,5 +1,33 @@
+from django.contrib.auth import login
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+from members.models import User
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'GET':
+        return render(request, 'index.html')
+
+    if request.method == 'POST':
+        email = request.POST['username']
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            return HttpResponse('이미 가입된 email입니다 :)')
+        if User.objects.filter(email=email).exists():
+            return HttpResponse('이미 사용중인 email입니다 :)')
+
+        user = User.objects.create_user(
+            password=password,
+            username=username,
+            email=email,
+            send_email=True
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('members:user-setting')
+
+        return redirect('members:login')
