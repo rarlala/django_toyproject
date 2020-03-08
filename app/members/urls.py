4a1cafaf2f74514f7d68021ae4348386d80df366
JEONGSUJI <wtw9611@gmail.com>
2020-03-09 03:34:10 +0900

    회원탈퇴 기본 구현

--- app/members/urls.py
+++ app/members/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path, include
 
-from members.views import login_view, user_setting_view, logout_view, send_email
+from members.views import login_view, user_setting_view, logout_view, send_email, withdrawal
 
 app_name = 'members'
 
@@ -10,8 +10,5 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('user-setting/', user_setting_view, name='user-setting'),
     path('send-email/', send_email, name='send-email'),
-
-    url(r'^django_popup_view_field/',
-        include('django_popup_view_field.urls', namespace="django_popup_view_field")
-        ),
+    path('withdrawal/', withdrawal, name='withdrawal'),
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -15,6 +16,11 @@ def login_view(request):
         if user is not None:
             login(request, user)
             return redirect('members:user-setting')
+
+        else:
+            messages.add_message(request, messages.ERROR, '가입되지 않은 정보입니다. 다시 확인 후 입력해주세요.')
+            return render(request, 'members/login.html')
+
     else:
         return render(request, 'members/login.html')
 
@@ -34,6 +40,14 @@ def logout_view(request):
 def send_email(request):
     email = request.user
     user = User.objects.get(email=email)
-    user.send_email = False if user.send_email==True else True
+    user.send_email = False if user.send_email == True else True
     user.save()
     return redirect('members:user-setting')
+
+
+# 회원탈퇴 처리
+def withdrawal(request):
+    print('회원탈퇴 함수 접근')
+    request.user.delete()
+    messages.add_message(request, messages.ERROR, '회원탈퇴가 완료되었습니다.')
+    return redirect('index')
